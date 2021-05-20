b548b59217572a3f8222044700bf2181f5b2d838
Peter Bruckner <peterbruckner08@gmail.com>
2021-05-20 21:48:10 +0930

    fixed index.js pathing

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.use(express.static('public'));
 
 // ROUTER
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
 app.get('/api/notes', (req, res) => res.json(db.notes));
 
 
